d8c02204b53e69754d5a6b9fda1bb918fa41363d
Satyadarshin Perry <stdn.engineer@gmail.com>
2019-11-25 16:14:04 +0000

    Footer: added privcay policy and link; adjusted and corrected display of text lines.

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <div id="twitter_sprite"><a target="_blank" href="https://www.youtube.com/user/tessabide"></a></div>
             <div id="youtube_sprite"><a target="_blank" href="https://twitter.com/tessabide"></a></div>
         </div>
-        <p id="credits"><span id="copyright">&copy; <?php echo get_bloginfo( 'name' ); echo " " . date('Y');?></span> <span id="programmer">Abide Theme:&nbsp;<a href="https://www.satyadarshin.com/">Satyadarshin</a></span></p>
+        <p id="credits"><span id="copyright">&copy; <?php echo get_bloginfo( 'name' ); echo "&nbsp;" . date('Y');?></span> <span id="programmer">Abide&nbsp;Theme: <a href="https://www.satyadarshin.com/" target="_blank">Satyadarshin</a></span><br><span id="privacy"><a href=" https://tessabide.com/privacy">Privacy policy</a></span></p>
     </footer>
 <?php wp_footer(); ?>
         </div> <!-- closes page container -->
