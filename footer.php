b8f3749c2fe522320a665a9f7c701967499c6b61
Satyadarshin Perry <stdn.engineer@gmail.com>
2020-02-07 15:09:13 +0000

    Minor corrections to styles, particlularly to the WP native gallery.

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
             <div id="facebook_sprite"><a target="_blank" href="https://www.facebook.com/tessabideproductions"></a></div>
             <div id="twitter_sprite"><a target="_blank" href="https://www.youtube.com/user/tessabide"></a></div>
             <div id="youtube_sprite"><a target="_blank" href="https://twitter.com/tessabide"></a></div>
+            <div id="instagram_sprite"><a target="_blank" href="https://www.instagram.com/tessabide/"></a></div>
         </div>
         <p id="credits"><span id="copyright">&copy; <?php echo get_bloginfo( 'name' ); echo "&nbsp;" . date('Y');?></span> <span id="programmer">Abide&nbsp;Theme: <a href="https://www.satyadarshin.com/" target="_blank">Satyadarshin</a></span><br><span id="privacy"><a href=" https://tessabide.com/privacy">Privacy policy</a></span></p>
     </footer>
--- functions.php
+++ functions.php
@@ -1,42 +1,47 @@
 <?php
-	/*
-	* Widget locations
-	*/
-	function abide_Widgets() {
-		register_sidebar(
-			array(
-				'name' => 'Sidebar',
-				'id' => 'sidebar_1',
-				'before_widget' => '<div>',
-				'after_widget' => '</div>',
-				'before_title' => '<h4>',
-				'after_title' => '</h4>'
-			)
-		);
-	}
-	add_action('widgets_init', 'abide_Widgets');
+/*
+ * Widget locations
+*/
+function abide_Widgets() {
+	register_sidebar(
+		array(
+			'name' => 'Sidebar',
+			'id' => 'sidebar_1',
+			'before_widget' => '<div>',
+			'after_widget' => '</div>',
+			'before_title' => '<h4>',
+			'after_title' => '</h4>'
+		)
+	);
+}
+add_action('widgets_init', 'abide_Widgets');
 
-	/*
-	* Makes a shotcode available for inserting responsive, embedded YouTube videos.
-	* Insert the shortcode [abide-youtube src=""] with the the url of the video as the attribute.
-	*/
-	function responsive_youtube_embed( $atts ) {
-		$embed = shortcode_atts( array(
-			'src' => 'something',
-		), $atts );
-		return "<div class=\"youtube-responsive-container\"><iframe src=\"{$embed['src']}\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe></div>";
-	}
-	add_shortcode( 'abide-youtube', 'responsive_youtube_embed' );
+/*
+ * Makes a shotcode available for inserting responsive, embedded YouTube videos.
+ * Insert the shortcode [abide-youtube src=""] with the the url of the video as the attribute.
+*/
+function responsive_youtube_embed( $atts ) {
+	$embed = shortcode_atts( array(
+		'src' => 'something',
+	), $atts );
+	return "<div class=\"youtube-responsive-container\"><iframe src=\"{$embed['src']}\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe></div>";
+}
+add_shortcode( 'abide-youtube', 'responsive_youtube_embed' );
 
-	function abide_theme_scripts() {
-		//Register script location, dependencies, and version
-		   wp_register_script(
-			   'abide_script',
-		   		get_template_directory_uri() . '/js/abide.js',
-		   		array('jquery')
-			);
-		 //Enqueue the script
-		  wp_enqueue_script('abide_script');
-		  }
-		add_action('wp_enqueue_scripts', 'abide_theme_scripts');
+function abide_theme_scripts() {
+//Register script location, dependencies, and version
+	wp_register_script(
+		'abide_script',
+		get_template_directory_uri() . '/js/abide.js',
+		array('jquery')
+	);
+	//Enqueue the script
+	wp_enqueue_script('abide_script');
+}
+add_action('wp_enqueue_scripts', 'abide_theme_scripts');
+
+/*
+ * Take control of WP Gallery styling 
+ * /
+add_filter( 'use_default_gallery_style', '__return_false' );
 ?>
