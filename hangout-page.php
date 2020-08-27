584c96f158849ef576f52c500f149d1c0696b500
Satyadarshin Perry <stdn.engineer@gmail.com>
2020-08-27 17:19:25 +0100

    Anarchist Librarian Hangout template complete

--- hangout-page.php
+++ hangout-page.php
@@ -35,19 +35,29 @@
         </aside>
     </section>
     <? if ( ! post_password_required() ): ?>
-    <section class="hangout area_a ">
+    <section class="hangout area_a two_columns">
         <article>
             <?php the_field( 'hangout_2' ); ?>
         </article>
     </section>
-    <section class="hangout area_b hangout_gallery">
+    <section class="hangout area_b two_columns">
         <article>
             <?php the_field( 'hangout_3' ); ?>
         </article>
     </section>
-    <section class="hangout area_a hangout_contact_capture">
+    <section class="hangout area_a">
         <article>
             <?php the_field( 'hangout_4' ); ?>
+        </article>
+    </section>
+    <section class="hangout area_b hangout_gallery ">
+        <article>
+            <?php the_field( 'hangout_5' ); ?>
+        </article>
+    </section>
+    <section class="hangout area_a hangout_contact_capture">
+        <article>
+            <?php the_field( 'hangout_6' ); ?>
         </article>
     </section>
     <? endif; ?>
